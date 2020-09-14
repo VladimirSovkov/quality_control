59a15167a2233ee6cad511490e0b18d63e84a2fc
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-09-14 14:36:30 +0300

    Add lab1

---
+++ lab1/Triangle/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        private static bool IsEquilateral(double a, double b, double c)
+        {
+            return a == b && b == c;
+        }
+
+        private static bool IsIsosceles(double a, double b, double c)
+        {
+            return a == b || b == c || a == c;
+        }
+
+        private static bool IsExistingTriangle(double a, double b, double c)
+        {
+            return a + b > c && b + c > a && a + c > b;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("неизвестная ошибка");
+                return;
+            }
+
+            try
+            {
+                double a = Convert.ToDouble(args[0]);
+                double b = Convert.ToDouble(args[1]);
+                double c = Convert.ToDouble(args[2]);
+                if (IsExistingTriangle(a, b, c))
+                {
+                    if (IsEquilateral(a, b, c))
+                    {
+                        Console.WriteLine("равносторонний");
+                    }
+                    else if (IsIsosceles(a, b, c))
+                    {
+                        Console.WriteLine("равнобедренный");
+                    }
+                    else
+                    {
+                        Console.WriteLine("обычный");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("не треугольник");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
+        }
+    }
+}
---
+++ lab1/TriangleTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TriangleTest
+{
+    public class Program
+    {
+        private static readonly string pathToExecutableFile = @"..\..\..\..\Triangle\bin\Release\netcoreapp3.1\";
+        private static readonly string pathToCheckResults = @"..\..\..\ResultOfCheck.txt";
+        private static bool CheckProgramResponse(string inputValues, string example)
+        {
+            ProcessStartInfo procStartInfo = new ProcessStartInfo(pathToExecutableFile + "Triangle.exe");
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = false;
+            procStartInfo.Arguments = inputValues;
+            Process proc = new Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd().Trim();
+            return result == example;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Некорректное кол-во элементов");
+                return;
+            }
+
+            try
+            {
+                string path = args[0];
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    using (StreamWriter sw = new StreamWriter(pathToCheckResults, false))
+                    {
+                        for (int count = 1; !sr.EndOfStream; count++)
+                        {
+                            string inputValues = sr.ReadLine();
+                            string example = sr.ReadLine();
+                            if (CheckProgramResponse(inputValues, example))
+                            {
+                                sw.WriteLine($"{count} success");
+                            }
+                            else
+                            {
+                                sw.WriteLine($"{count} error");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
